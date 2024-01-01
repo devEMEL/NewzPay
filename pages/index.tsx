0f4db44f249320ba8158ee28b55de0a11a161bd3
devEMEL <dominichenry882@gmail.com>
2024-01-01 13:28:14 +0100

    fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,21 @@
 import AddProductModal from "@/components/AddProductModal";
 import Layout from "@/components/Layout";
 import ProductList from "@/components/ProductList";
+import Head from "next/head";
 
 // Export the Home component
 export default function Home() {
-    return (
-        <div>
-            <AddProductModal />
-            <ProductList />
-        </div>
-    );
+  return (
+    <div>
+      <Head>
+        <title>NewzPay</title>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        ></meta>
+      </Head>
+      <AddProductModal />
+      <ProductList />
+    </div>
+  );
 }
